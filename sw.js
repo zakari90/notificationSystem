14e9d7e231722ff15c5035171a242aa3d24f0405
zakari90 <zakariazinedine1@gmail.com>
2024-08-08 19:21:53 +0100

    first commit

--- sw.js
+++ sw.js
@@ -19,20 +19,37 @@
 //     );
 // });
 
-self.addEventListener('activate', async () => {
+  const subscription = await self.registration.pushManager.getSubscription()
+
+// urlB64ToUint8Array is a magic function that will encode the base64 public key
+// to Array buffer which is needed by the subscription option
+const urlB64ToUint8Array = base64String => {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+    const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
+    const rawData = atob(base64)
+    const outputArray = new Uint8Array(rawData.length)
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i)
+    }
+    return outputArray
+  }
+  self.addEventListener('activate', async () => {
     // This will be called only once when the service worker is activated.
-console.log("hello from ------self.addEventListener('activate' ----- ");
-    
     try {
-      const options = {}
+      const applicationServerKey = urlB64ToUint8Array(publicKey)
+      const options = { applicationServerKey, userVisibleOnly: true }
       const subscription = await self.registration.pushManager.subscribe(options)
-      console.log("----------------"+JSON.stringify(subscription))
+      console.log("addEventListener('activate -->"+ JSON.stringify(subscription))
     } catch (err) {
       console.log('Error', err)
     }
   })
 
-  const subscription = await self.registration.pushManager.getSubscription()
-
 console.log("subscription ----->" + subscription);
 console.log("Service Worker ended");
+
+
+const publicKey = "BHHMHf-ODDQ99rS7-tyHt0QnB1-ntsVzFvebI0f4zZocT5kpYwsE-fZlws9_WJscx8NQ_cioihK8BLqABDhTw1g"
+
+// Private Key:
+// BUchHAT99c4QBInXF3T2hQ4aIym_1zastM4b0Yfk1E0
